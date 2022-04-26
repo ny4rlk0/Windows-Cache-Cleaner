665b87a9f8a73c4b7621a2b0ec832434bb2a9cc4
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2022-04-27 00:25:10 +0300

    Update PartlyRestoreHijackedSystem.bat

--- PartlyRestoreHijackedSystem.bat
+++ PartlyRestoreHijackedSystem.bat
@@ -1,6 +1,8 @@
 TITLE Partly Restore Hijacked System
 cls
 echo "This will not work if you did not removed virus and thing that autostarts it!"
+echo "This computer needs to be connected to internet so this batch file repairs the Windows!"
+echo "It will download required files from Microsoft's server!"
 echo "Ny4rlk0 https://github.com/ny4rlk0/Windows-Cache-Cleaner/"
 pause
 @echo off
