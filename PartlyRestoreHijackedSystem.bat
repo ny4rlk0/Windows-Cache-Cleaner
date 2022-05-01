8e4f5a5effa275b9520af1ae869adc4e23ff4632
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2022-05-01 18:50:34 +0300

    Update PartlyRestoreHijackedSystem.bat

--- PartlyRestoreHijackedSystem.bat
+++ PartlyRestoreHijackedSystem.bat
@@ -1,13 +1,12 @@
 TITLE Partly Restore Hijacked System
 cls
-echo "This will not work if you did not removed virus and thing that autostarts it!"
-echo "This computer needs to be connected to internet so this batch file repairs the Windows!"
-echo "It will download required files from Microsoft's server!"
-echo "Ny4rlk0 https://github.com/ny4rlk0/Windows-Cache-Cleaner/"
-echo " "
-echo "Press Enter to continue!"
-pause
 @echo off
+echo This will not work if you did not removed virus and thing that autostarts it!
+echo This computer needs to be connected to internet so this batch file repairs the Windows!
+echo It will download required files from Microsoft's server!
+echo Ny4rlk0 https://github.com/ny4rlk0/Windows-Cache-Cleaner/
+echo 
+pause
 if _%1_==_payload_  goto :payload
 
 :getadmin
@@ -22,13 +21,25 @@ goto :eof
 :payload
 
 ::Kodu buraya yaz::
-sc config "SysMain" start=auto
+powershell "gwmi win32_process | select CommandLine | select-string -pattern ".exe" > "%HOMEDRIVE%\users\%username%\Desktop\ProccessDump.txt"
+ipconfig /displaydns >> %HOMEDRIVE%\users\%username%\Desktop\DNSDump.txt
 RD /S /Q "%WinDir%\System32\GroupPolicy"
 RD /S /Q "%WinDir%\System32\GroupPolicyUsers"
 gpupdate /force
+sc config "SysMain" start=auto
 sc config "UsoSvc" start=auto
 sc config "wuauserv" start=auto
 sc config "WaasMedicSvc" start=auto
+sc config "mpssvc" start=auto
+sc config "SecurityHealthService" start=auto
+sc config "DoSvc" start=auto
+sc config "SgrmBroker" start=auto
+sc config "uhssvc" start=auto
+sc config "InstallService" start=auto
+sc config "WinDefend" start=auto
+sc config "WdNisSvc" start=auto
+sc config "WlanSvc" start=auto
+sc config "wscsvc" start=auto
 netsh interface tcp set global autotuninglevel=highlyrestricted
 REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\PolicyAgent" /v "AssumeUDPEncapsulationContextOnSendRule" REG_DWORD /d 2 /f
 ipconfig /flushdns
@@ -40,6 +51,15 @@ sc start "UsoSvc"
 sc start "wuauserv"
 sc start "WaasMedicSvc"
 sc start "SysMain"
+sc start "mpssvc"
+sc start "DoSvc"
+sc start "SgrmBroker"
+sc start "uhssvc"
+sc start "InstallService"
+sc start "WinDefend"
+sc start "WdNisSvc"
+sc start "WdNisSvc"
+sc start "wscsvc"
 DISM /Online /Cleanup-Image /CheckHealth
 DISM /Online /Cleanup-Image /RestoreHealth
 shutdown -r -t 0
