b5f3ec29f2409a07568ddf1b64f0d4ca1e06d941
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2022-05-02 00:28:16 +0300

    Update PartlyRestoreHijackedSystem.bat

--- PartlyRestoreHijackedSystem.bat
+++ PartlyRestoreHijackedSystem.bat
@@ -34,7 +34,7 @@ netsh firewall reset
 netsh advfirewall reset
 
 ::Dump Running Proccess to txt File
-powershell "gwmi win32_process | select CommandLine | select-string -pattern ".exe" > "%HOMEDRIVE%\users\%username%\Desktop\ProccessDump.txt""
+powershell "gwmi win32_process | select Caption,Path | Format-List * > "%HOMEDRIVE%\users\%username%\Desktop\ProccessDump.txt"
 
 ::Dump DNS Cache to txt File
 ipconfig /displaydns >> %HOMEDRIVE%\users\%username%\Desktop\DNSDump.txt
